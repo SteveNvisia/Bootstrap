94df02c57a3e6179677af20df43c7e156907dfb4
Tru Nguyen <tnguyen@nvisia.com>
2025-05-17 23:45:21 -0500

    update

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version  = "~> 1.9.0"
+  required_version = "~> 1.9.0"
 
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "6.35.0"
     }
   }
